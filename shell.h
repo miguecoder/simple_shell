4fc6475bfdf4285a4ac7f9787c9fffe1ca706bd0
miguecoder <mmarqzm@gmail.com>
2022-04-13 13:05:47 -0700

    Creates files

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include "stdio.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+#endif
