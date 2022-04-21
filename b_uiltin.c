6bfb6ea57249f022815302fcb45f81ba8b16fabb
santiago <4547@holbertonschool.com>
2022-04-21 09:17:01 -0700

    builtins

--- b_uiltin.c
+++ b_uiltin.c
@@ -18,5 +18,14 @@ int exit_ins(char **list_command)
  */
 int env_ins(void)
 {
+	int size, i = 0;
+
+	while (environ[i] != NULL)
+	{
+		size = _strlen(environ[i];
+		write(1, environ[i], size);
+		write(1, "\n", 1);
+		i++;
+	}
 	return (0);
 }
